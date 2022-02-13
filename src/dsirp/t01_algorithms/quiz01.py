871accf60121d5415db0e02d6d953bfb4c8fae83
wilsonify <tom.a.wilson@gmail.com>
2022-02-13 11:26:16 -0500

    add if __name__ == "__main__"

--- src/dsirp/t01_algorithms/quiz01.py
+++ src/dsirp/t01_algorithms/quiz01.py
@@ -35,110 +35,98 @@
 # +
 from os.path import basename, exists
 
+
 def download(url):
     filename = basename(url)
     if not exists(filename):
         from urllib.request import urlretrieve
         local, _ = urlretrieve(url, filename)
         print('Downloaded ' + local)
-    
-download('https://github.com/AllenDowney/DSIRP/raw/main/american-english')
 
 
-# -
-
-def read_words(filename):
-    """Read lines from a file and split them into words."""
-    res = set()
-    for line in open(filename):
-        for word in line.split():
-            res.add(word.strip().lower())
-    return res
+if __name__ == "__main__":
+    download('https://github.com/AllenDowney/DSIRP/raw/main/american-english')
 
 
-word_list = read_words('american-english')
-len(word_list)
+    # -
+
+    def read_words(filename):
+        """Read lines from a file and split them into words."""
+        res = set()
+        for line in open(filename):
+            for word in line.split():
+                res.add(word.strip().lower())
+        return res
 
 
-# ## Question 1
-#
-# The following function takes a string and returns `True` if the letters in the string appear in alphabetical order.
-
-def is_alphabetical(word):
-    return list(word) == sorted(word)
+    word_list = read_words('american-english')
+    len(word_list)
 
 
-is_alphabetical('almost') # True
+    # ## Question 1
+    #
+    # The following function takes a string and returns `True` if the letters in the string appear in alphabetical order.
 
-is_alphabetical('alphabetical') # False
-
-# Make a new list called `alpha_words` that contains only the words in `word_list` that are alphabetical, and display the length of the list.
-
-
-
+    def is_alphabetical(word):
+        return list(word) == sorted(word)
 
 
-# ## Question 2
-#
-# Find and display the longest word in `alpha_words`.
-# If there is more than one word with the maximal length, you can display any one of them (but only one).
-#
-# NOTE: You can write code for this question even if your answer to the previous question doesn't work. I'll grade the code, not the output.
+    is_alphabetical('almost')  # True
+
+    is_alphabetical('alphabetical')  # False
+
+    # Make a new list called `alpha_words` that contains only the words in `word_list` that are alphabetical, and display the length of the list.
+
+    # ## Question 2
+    #
+    # Find and display the longest word in `alpha_words`.
+    # If there is more than one word with the maximal length, you can display any one of them (but only one).
+    #
+    # NOTE: You can write code for this question even if your answer to the previous question doesn't work. I'll grade the code, not the output.
+
+    # ## Question 3
+    #
+    # Write a function called `encompasses` that takes two words and returns `True` if the first word contains the second word, but not at the beginning or the end (and `False` otherwise). For example, `hippopotomus` encompasses the word `pot`.
+    #
+    # HINT: You might find the string method `find` useful.
+
+    'hippopotomus'.find('pot')
+
+    'hippopotomus'.find('potato')
+
+    def encompasses():
+        """
+        takes two words and returns `True` if the first word contains the second word,
+        but not at the beginning or the end (and `False` otherwise).
+        For example, `hippopotomus` encompasses the word `pot`.
+        """
+        return True
 
 
+    # You can use the following examples to test your function.
 
+    word1 = 'hippopotamus'
+    word2 = 'pot'
+    word3 = 'hippo'
+    word4 = 'mus'
+    word5 = 'potato'
 
+    encompasses(word1, word2)  # True
 
-# ## Question 3
-#
-# Write a function called `encompasses` that takes two words and returns `True` if the first word contains the second word, but not at the beginning or the end (and `False` otherwise). For example, `hippopotomus` encompasses the word `pot`.
-#
-# HINT: You might find the string method `find` useful. 
+    encompasses(word1, word3)  # False because word3 is at the beginning
 
-'hippopotomus'.find('pot')
+    encompasses(word1, word4)  # False because word4 is at the end
 
-'hippopotomus'.find('potato')
+    encompasses(word1, word5)  # False because word5 is not in word1
 
-# +
-# WRITE YOUR FUNCTION HERE
-# -
+    # ## Question 4
+    #
+    # Two words make a "reverse pair" if one of them is the reverse of the other. For example, `pots` and `stop` are a reverse pair.
+    #
+    # The words in a reverse pair must be different, so `gag` and `gag` are not a reverse pair.
+    #
+    #
+    # Make a list of all reverse pairs in `word_list`. Each pair of words should appear only once, so if `('tons', 'snot')` is in the list, `('snot', 'tons')` should not be.
+    #
 
-
-
-
-
-
-
-# You can use the following examples to test your function.
-
-word1 = 'hippopotamus'
-word2 = 'pot'
-word3 = 'hippo'
-word4 = 'mus'
-word5 = 'potato'
-
-encompasses(word1, word2) # True
-
-encompasses(word1, word3) # False because word3 is at the beginning
-
-encompasses(word1, word4) # False because word4 is at the end
-
-encompasses(word1, word5) # False because word5 is not in word1
-
-# ## Question 4
-#
-# Two words make a "reverse pair" if one of them is the reverse of the other. For example, `pots` and `stop` are a reverse pair.
-#
-# The words in a reverse pair must be different, so `gag` and `gag` are not a reverse pair.
-#
-#
-# Make a list of all reverse pairs in `word_list`. Each pair of words should appear only once, so if `('tons', 'snot')` is in the list, `('snot', 'tons')` should not be.
-#
-
-
-
-# BONUS QUESTION JUST FOR FUN: What is the longest reverse pair in this word list?
-
-
-
-
+    # BONUS QUESTION JUST FOR FUN: What is the longest reverse pair in this word list?
--- src/dsirp/t01_algorithms/timing.py
+++ src/dsirp/t01_algorithms/timing.py
@@ -27,287 +27,295 @@
 # +
 import os
 
+
 def etime():
     """Measures user and system time this process has used.
 
     Returns the sum of user and system time."""
     user, sys, chuser, chsys, real = os.times()
-    return user+sys
+    return user + sys
 
 
-# -
+if __name__ == "__main__":
 
-start = etime()
-t = [x**2 for x in range(10000)]
-end = etime()
-end - start
+    # -
 
-# Exercise: Use `etime` to measure the computation time used by `sleep`.
-
-# +
-from time import sleep
-
-sleep(1)
-# -
-
-
-
-def time_func(func, n):
-    """Run a function and return the elapsed time.
-
-    func: function
-    n: problem size, passed as an argument to func
-
-    returns: user+sys time in seconds
-    """
     start = etime()
-    func(n)
+    t = [x ** 2 for x in range(10000)]
     end = etime()
-    elapsed = end - start
-    return elapsed
+    end - start
+
+    # Exercise: Use `etime` to measure the computation time used by `sleep`.
+
+    # +
+    from time import sleep
+
+    sleep(1)
 
 
-# One of the things that makes timing tricky is that many operations are too fast to measure accurately.
-#
-# `%timeit` handles this by running enough times get a precise estimate, even for things that run very fast.
-#
-# We'll handle it by running over a wide range of problem sizes, hoping to sizes that run long enough to measure, but not more than a few seconds.
+    # -
 
-#
-# The following function takes a size, `n`, creates an empty list, and calls `list.append` `n` times.
+    def time_func(func, n):
+        """Run a function and return the elapsed time.
 
-def list_append(n):
-    t = []
-    [t.append(x) for x in range(n)]
+        func: function
+        n: problem size, passed as an argument to func
 
-
-# `timeit` can time this function accurately.
-
-list_append(10000)
-
-# But our `time_func` is not that smart.
-
-time_func(list_append, 10000)
-
-# Exercise: Increase the number of iterations until the run time is measureable.
+        returns: user+sys time in seconds
+        """
+        start = etime()
+        func(n)
+        end = etime()
+        elapsed = end - start
+        return elapsed
 
 
+    # One of the things that makes timing tricky is that many operations are too fast to measure accurately.
+    #
+    # `%timeit` handles this by running enough times get a precise estimate, even for things that run very fast.
+    #
+    # We'll handle it by running over a wide range of problem sizes, hoping to sizes that run long enough to measure, but not more than a few seconds.
 
-# ## List append
-#
-# The following function gradually increases `n` and records the total time.
+    #
+    # The following function takes a size, `n`, creates an empty list, and calls `list.append` `n` times.
 
-def run_timing_test(func, max_time=1):
-    """Tests the given function with a range of values for n.
-
-    func: function object
-
-    returns: list of ns and a list of run times.
-    """
-    ns = []
-    ts = []
-    for i in range(10, 28):
-        n = 2**i
-        t = time_func(func, n)
-        print(n, t)
-        if t > 0:
-            ns.append(n)
-            ts.append(t)
-        if t > max_time:
-            break
-
-    return ns, ts
+    def list_append(n):
+        t = []
+        [t.append(x) for x in range(n)]
 
 
-ns, ts = run_timing_test(list_append)
+    # `timeit` can time this function accurately.
 
-# +
-import matplotlib.pyplot as plt
+    list_append(10000)
 
-plt.plot(ns, ts, 'o-')
-plt.xlabel('Problem size (n)')
-plt.ylabel('Runtime (seconds)');
+    # But our `time_func` is not that smart.
 
-
-# -
-
-# This one looks pretty linear, but it won't always be so clear.
-# It will help to plot a straight line that goes through the last data point.
-
-def fit(ns, ts, exp=1.0, index=-1):
-    """Fits a curve with the given exponent.
-
-    ns: sequence of problem sizes
-    ts: sequence of times
-    exp: exponent of the fitted curve
-    index: index of the element the fitted line should go through
-
-    returns: sequence of fitted times
+    time_func(list_append, 10000)
 
 
-    """
-    # Use the element with the given index as a reference point,
-    # and scale all other points accordingly.
-    nref = ns[index]
-    tref = ts[index]
+    # Exercise: Increase the number of iterations until the run time is measureable.
 
-    tfit = []
-    for n in ns:
-        ratio = n / nref
-        t = ratio**exp * tref
-        tfit.append(t)
+    # ## List append
+    #
+    # The following function gradually increases `n` and records the total time.
 
-    return tfit
+    def run_timing_test(func, max_time=1):
+        """Tests the given function with a range of values for n.
+
+        func: function object
+
+        returns: list of ns and a list of run times.
+        """
+        ns = []
+        ts = []
+        for i in range(10, 28):
+            n = 2 ** i
+            t = time_func(func, n)
+            print(n, t)
+            if t > 0:
+                ns.append(n)
+                ts.append(t)
+            if t > max_time:
+                break
+
+        return ns, ts
 
 
-ts_fit = fit(ns, ts)
-ts_fit
+    ns, ts = run_timing_test(list_append)
 
+    # +
+    import matplotlib.pyplot as plt
 
-# The following function plots the actual results and the fitted line.
-
-def plot_timing_test(ns, ts, label='', color='C0', exp=1.0, scale='log'):
-    """Plots data and a fitted curve.
-
-    ns: sequence of n (problem size)
-    ts: sequence of t (run time)
-    label: string label for the data curve
-    color: string color for the data curve
-    exp: exponent (slope) for the fitted curve
-    scale: string passed to xscale and yscale
-    """
-    ts_fit = fit(ns, ts, exp)
-    fit_label = 'exp = %d' % exp
-    plt.plot(ns, ts_fit, label=fit_label, color='0.7', linestyle='dashed')
-    plt.plot(ns, ts, 'o-', label=label, color=color, alpha=0.7)
+    plt.plot(ns, ts, 'o-')
     plt.xlabel('Problem size (n)')
-    plt.ylabel('Runtime (seconds)')
-    plt.xscale(scale)
-    plt.yscale(scale)
-    plt.legend()
+    plt.ylabel('Runtime (seconds)');
 
 
-plot_timing_test(ns, ts, scale='linear')
-plt.title('list append');
+    # -
 
-# From these results, what can we conclude about the order of growth of `list.append`?
+    # This one looks pretty linear, but it won't always be so clear.
+    # It will help to plot a straight line that goes through the last data point.
 
-# Before we go on, let's also look at the results on a log-log scale.
+    def fit(ns, ts, exp=1.0, index=-1):
+        """Fits a curve with the given exponent.
 
-plot_timing_test(ns, ts, scale='log')
-plt.title('list append');
+        ns: sequence of problem sizes
+        ts: sequence of times
+        exp: exponent of the fitted curve
+        index: index of the element the fitted line should go through
 
-
-# Why might we prefer this scale?
-
-# ## List pop
-#
-# Now let's do the same for `list.pop` (which pops from the end of the list by default).
-#
-# Notice that we have to make the list before we pop things from it, so we will have to think about how to interpret the results.
-
-# +
-def list_pop(n):
-    t = []
-    [t.append(x) for x in range(n)]
-    [t.pop() for _ in range(n)]
-
-ns, ts = run_timing_test(list_pop)
-plot_timing_test(ns, ts, scale='log')
-plt.title('list pop');
+        returns: sequence of fitted times
 
 
-# -
+        """
+        # Use the element with the given index as a reference point,
+        # and scale all other points accordingly.
+        nref = ns[index]
+        tref = ts[index]
 
-# What can we conclude?
-#
-# What about `pop(0)`, which pops from the beginning of the list?
-#
-# Note: You might have to adjust `exp` to make the fitted line fit.
+        tfit = []
+        for n in ns:
+            ratio = n / nref
+            t = ratio ** exp * tref
+            tfit.append(t)
 
-# +
-def list_pop0(n):
-    t = []
-    [t.append(x) for x in range(n)]
-    [t.pop(0) for _ in range(n)]
-
-ns, ts = run_timing_test(list_pop0)
-plot_timing_test(ns, ts, scale='log', exp=1)
-plt.title('list pop(0)');
+        return tfit
 
 
-# -
-
-# ## Searching a list
-#
-# `list.index` searches a list and returns the index of the first element that matches the target.
-#
-# What do we expect if we always search for the first element?
-
-# +
-def list_index0(n):
-    t = []
-    [t.append(x) for x in range(n)]
-    [t.index(0) for _ in range(n)]
-
-ns, ts = run_timing_test(list_index0)
-
-plot_timing_test(ns, ts, scale='log', exp=1)
-plt.title('list index(0)');
+    ts_fit = fit(ns, ts)
+    ts_fit
 
 
-# -
+    # The following function plots the actual results and the fitted line.
 
-# What if we always search for the last element?
+    def plot_timing_test(ns, ts, label='', color='C0', exp=1.0, scale='log'):
+        """Plots data and a fitted curve.
 
-# +
-def list_index_n(n):
-    t = []
-    [t.append(x) for x in range(n)]
-    [t.index(n-1) for _ in range(n)]
-
-ns, ts = run_timing_test(list_index_n)
-
-plot_timing_test(ns, ts, scale='log', exp=1)
-plt.title('list index(n-1)');
-
-
-# -
-
-# ## Dictionary add
-
-# +
-def dict_add(n):
-    d = {}
-    [d.setdefault(x, x) for x in range(n)]
-
-ns, ts = run_timing_test(dict_add)
-
-plot_timing_test(ns, ts, scale='log', exp=1)
-plt.title('dict add');
+        ns: sequence of n (problem size)
+        ts: sequence of t (run time)
+        label: string label for the data curve
+        color: string color for the data curve
+        exp: exponent (slope) for the fitted curve
+        scale: string passed to xscale and yscale
+        """
+        ts_fit = fit(ns, ts, exp)
+        fit_label = 'exp = %d' % exp
+        plt.plot(ns, ts_fit, label=fit_label, color='0.7', linestyle='dashed')
+        plt.plot(ns, ts, 'o-', label=label, color=color, alpha=0.7)
+        plt.xlabel('Problem size (n)')
+        plt.ylabel('Runtime (seconds)')
+        plt.xscale(scale)
+        plt.yscale(scale)
+        plt.legend()
 
 
-# -
+    plot_timing_test(ns, ts, scale='linear')
+    plt.title('list append');
 
-# ## Dictionary lookup 
+    # From these results, what can we conclude about the order of growth of `list.append`?
 
-# +
-def dict_lookup(n):
-    d = {}
-    [d.setdefault(x, x) for x in range(n)]
-    [d[x] for x in range(n)]
+    # Before we go on, let's also look at the results on a log-log scale.
 
-ns, ts = run_timing_test(dict_lookup)
+    plot_timing_test(ns, ts, scale='log')
+    plt.title('list append');
 
-plot_timing_test(ns, ts, scale='log', exp=1)
-plt.title('dict lookup');
-# -
 
-# This characteristic of dictionaries is the foundation of a lot of efficient algorithms!
+    # Why might we prefer this scale?
 
-# + [markdown] tags=[]
-# *Data Structures and Information Retrieval in Python*
-#
-# Copyright 2021 Allen Downey
-#
-# License: [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+    # ## List pop
+    #
+    # Now let's do the same for `list.pop` (which pops from the end of the list by default).
+    #
+    # Notice that we have to make the list before we pop things from it, so we will have to think about how to interpret the results.
+
+    # +
+    def list_pop(n):
+        t = []
+        [t.append(x) for x in range(n)]
+        [t.pop() for _ in range(n)]
+
+
+    ns, ts = run_timing_test(list_pop)
+    plot_timing_test(ns, ts, scale='log')
+    plt.title('list pop');
+
+
+    # -
+
+    # What can we conclude?
+    #
+    # What about `pop(0)`, which pops from the beginning of the list?
+    #
+    # Note: You might have to adjust `exp` to make the fitted line fit.
+
+    # +
+    def list_pop0(n):
+        t = []
+        [t.append(x) for x in range(n)]
+        [t.pop(0) for _ in range(n)]
+
+
+    ns, ts = run_timing_test(list_pop0)
+    plot_timing_test(ns, ts, scale='log', exp=1)
+    plt.title('list pop(0)');
+
+
+    # -
+
+    # ## Searching a list
+    #
+    # `list.index` searches a list and returns the index of the first element that matches the target.
+    #
+    # What do we expect if we always search for the first element?
+
+    # +
+    def list_index0(n):
+        t = []
+        [t.append(x) for x in range(n)]
+        [t.index(0) for _ in range(n)]
+
+
+    ns, ts = run_timing_test(list_index0)
+
+    plot_timing_test(ns, ts, scale='log', exp=1)
+    plt.title('list index(0)');
+
+
+    # -
+
+    # What if we always search for the last element?
+
+    # +
+    def list_index_n(n):
+        t = []
+        [t.append(x) for x in range(n)]
+        [t.index(n - 1) for _ in range(n)]
+
+
+    ns, ts = run_timing_test(list_index_n)
+
+    plot_timing_test(ns, ts, scale='log', exp=1)
+    plt.title('list index(n-1)');
+
+
+    # -
+
+    # ## Dictionary add
+
+    # +
+    def dict_add(n):
+        d = {}
+        [d.setdefault(x, x) for x in range(n)]
+
+
+    ns, ts = run_timing_test(dict_add)
+
+    plot_timing_test(ns, ts, scale='log', exp=1)
+    plt.title('dict add');
+
+
+    # -
+
+    # ## Dictionary lookup
+
+    # +
+    def dict_lookup(n):
+        d = {}
+        [d.setdefault(x, x) for x in range(n)]
+        [d[x] for x in range(n)]
+
+
+    ns, ts = run_timing_test(dict_lookup)
+
+    plot_timing_test(ns, ts, scale='log', exp=1)
+    plt.title('dict lookup');
+    # -
+
+    # This characteristic of dictionaries is the foundation of a lot of efficient algorithms!
+
+    # + [markdown] tags=[]
+    # *Data Structures and Information Retrieval in Python*
+    #
+    # Copyright 2021 Allen Downey
+    #
+    # License: [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-nc-sa/4.0/)
